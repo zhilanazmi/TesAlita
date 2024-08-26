1160c2b3584161f593482352eecde78d287e927b
zhilanazmi <zhillan.azmi@mhs.itenas.ac.id>
2024-08-26 22:15:43 +0700

    fix: fix display Employees data on dashboard

--- app/Filament/Resources/EmployeesResource.php
+++ app/Filament/Resources/EmployeesResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EmployeesResource\Pages;
-use App\Filament\Resources\EmployeesResource\RelationManagers;
-use App\Models\Employees;
 use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+use App\Models\Employees;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\EmployeesResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\EmployeesResource\RelationManagers;
 
 class EmployeesResource extends Resource
 {
@@ -23,7 +26,14 @@ class EmployeesResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')->required(),
+                        TextInput::make('location_code')->required(),
+                        TextInput::make('birth_date')->required()
+                                    ->placeholder('YYYY-MM-DD'),
+                    ])
+                    ->columns(2),
             ]);
     }
 
@@ -31,13 +41,16 @@ class EmployeesResource extends Resource
     {
         return $table
             ->columns([
-                //
-            ])
+                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('location_code')->sortable()->searchable(),
+                TextColumn::make('birth_date')->sortable()->searchable(),
+                ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
